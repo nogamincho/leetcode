6b71499e43fd5ba9ececcc7fabd0e50e6bba928c
nogamincho <nogamincho@gmail.com>
2019-06-24 09:54:40 +0900

    good

--- src/main/java/nogamincho/interview/w_162/Solution.java
+++ src/main/java/nogamincho/interview/w_162/Solution.java
@@ -1,5 +1,17 @@
 package nogamincho.interview.w_162;
 
-class Solution {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+class Solution {
+    public int findPeakElement(int[] nums) {
+        List<Integer> arrayList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        int i = Arrays.stream(nums).max().orElse(Integer.MIN_VALUE);
+
+
+        return arrayList.lastIndexOf(i);
+
+    }
 }
